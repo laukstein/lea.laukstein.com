8f708cf6828beb572c7b8f39208dff4e5356900a
Binyamin <7raivis@inbox.lv>
2016-07-19 01:14:07 +0300

    ESLint rules

--- assets/escort.js
+++ assets/escort.js
@@ -1,3 +1,31 @@
+/*eslint
+comma-spacing: 2,
+dot-notation: [2, {"allowKeywords": true}],
+eqeqeq: 2,
+indent: 2,
+key-spacing: [2, {"beforeColon": false, "afterColon": true}],
+no-console: 0,
+no-empty-function: 2,
+no-eval: 2,
+no-extend-native: 2,
+no-inner-declarations: 2,
+no-loop-func: 2,
+no-mixed-spaces-and-tabs: 2,
+no-multi-spaces: 2,
+no-new-func: 2,
+no-new: 2,
+no-shadow: 2,
+no-trailing-spaces: "error",
+no-undef: 0,
+no-underscore-dangle: 2,
+no-unused-vars: 2,
+no-use-before-define: 2,
+quotes: [2, "double"],
+semi: 2,
+space-before-blocks: 2,
+space-before-function-paren: [2, {"anonymous": "always", "named": "never"}],
+strict: [2, "function"]*/
+
 var escort = {
     schema: {
         duration: 7,
@@ -31,7 +59,7 @@ var escort = {
 
         return param ? obj[param] : obj;
     },
-    diff: function() {
+    diff: function () {
         "use strict";
 
         try {
