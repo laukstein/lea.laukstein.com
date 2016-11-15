833d43d469d4f41f0d2674118441627c9ff8cfb3
Binyamin <7raivis@inbox.lv>
2016-11-15 23:42:00 +0200

    Typo

--- assets/ui-subscribe.js
+++ assets/ui-subscribe.js
@@ -37,7 +37,7 @@ ui.subscribe = {
 
         if (data) {
             if (data.result === "success") {
-                message = "<h1 class=success>תודה</h1><p>לסיום ההרשמה תקבל עכשיו מייל לאישור</p>";
+                message = "<h1 class=success>תודה</h1><p>לסיום ההרשמה תקבלי עכשיו מייל לאישור</p>";
             } else if (data.msg && data.msg.indexOf("list-manage.com/subscribe/send-email")) {
                 message = "<h1 class=error>שגיאה...</h1><p>את כבר רשומה, תנסי עם <a href=" + location.href + ">אימייל אחר</a></p>";
             } else {
