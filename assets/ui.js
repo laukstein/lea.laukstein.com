6bc8b6bd74ceafd5e9bff6fa49582042790318fa
Binyamin <7raivis@inbox.lv>
2018-04-09 22:07:20 +0300

    Raven.js v3.24.1

--- assets/ui.js
+++ assets/ui.js
@@ -199,7 +199,7 @@ window.ui = {
             });
 
             // Sentry JavaScript client Raven.js https://docs.sentry.io/clients/javascript/install/
-            this.asyncScript("https://cdn.ravenjs.com/3.24.0/raven.min.js", {
+            this.asyncScript("https://cdn.ravenjs.com/3.24.1/raven.min.js", {
                 // Generator SRI hash https://www.srihash.org
                 integrity: "sha384-1y+l9bVzUA1/76o95b5ULcZBo0OrSvxTDaLvzKV23VYCWpjaF89QaOuD4nN8Lzsy",
                 crossorigin: "anonymous",
