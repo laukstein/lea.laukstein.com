4b31ec1141296b3bf8d0b40219fb9ab10949a7e4
Binyamin <7raivis@inbox.lv>
2018-11-18 20:29:23 +0200

    Removed backwards compatibility - redirecting to /order page

--- assets/ui-colorSwatch.js
+++ assets/ui-colorSwatch.js
@@ -1,39 +1,34 @@
 ui.colorSwatch = (function () {
     "use strict";
 
-    if (ui.hash({param: "transaction"})) {
-        // Backwards compatibility due to renamed URLs
-        ui.w.location = "/order#" + ui.serialize(ui.hash());
-    } else {
-        var buttons = Array.from(ui.d.querySelectorAll(".x-button")),
-            hash = ui.hash({hash: location.search}),
-            baseurl = buttons[0] && buttons[0].href,
-            urlParams = ["affiliate", "campaign"],
-            filterObj,
-            query;
+    var buttons = Array.from(ui.d.querySelectorAll(".x-button")),
+        hash = ui.hash({hash: location.search}),
+        baseurl = buttons[0] && buttons[0].href,
+        urlParams = ["affiliate", "campaign"],
+        filterObj,
+        query;
 
-        if (baseurl && baseurl.indexOf("#") && Object.keys(hash).length) {
-            filterObj = function (raw, condition) {
-                return Object.keys(raw).filter(function (key) {
-                    return condition(key, raw);
-                }).reduce(function (obj, key) {
-                    obj[key] = raw[key];
+    if (baseurl && baseurl.indexOf("#") && Object.keys(hash).length) {
+        filterObj = function (raw, condition) {
+            return Object.keys(raw).filter(function (key) {
+                return condition(key, raw);
+            }).reduce(function (obj, key) {
+                obj[key] = raw[key];
 
-                    return obj;
-                }, {});
-            };
+                return obj;
+            }, {});
+        };
 
-            query = ui.serialize(filterObj(hash, function (key) {
-                return urlParams.includes(key);
-            }));
+        query = ui.serialize(filterObj(hash, function (key) {
+            return urlParams.includes(key);
+        }));
 
-            if (query) {
-                baseurl += "&" + query;
+        if (query) {
+            baseurl += "&" + query;
 
-                buttons.forEach(function (button) {
-                    button.href = baseurl;
-                });
-            }
+            buttons.forEach(function (button) {
+                button.href = baseurl;
+            });
         }
     }
 }());
