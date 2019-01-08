6b432bec95028817b6904b1fbb170137bf9065a8
Binyamin <7raivis@inbox.lv>
2019-01-08 23:06:17 +0200

    ui.isObj(str) - String parsed to Object/Array

--- assets/ui.js
+++ assets/ui.js
@@ -425,6 +425,17 @@ window.ui = {
             // Isn't a phone number that begins with "0"
             (String(str).length === String(Number(str)).length || str.indexOf(".") > 0);
     },
+    isObj: function (str) {
+        "use strict";
+
+        try {
+            var obj = JSON.parse(str);
+
+            return typeof obj === "object";
+        } catch (e) {
+            return false;
+        }
+    },
     hash: function (options /* param */) {
         "use strict";
 
