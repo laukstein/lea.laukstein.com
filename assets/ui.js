2aa47b548764fe1e0c403f103b896d62f55ab264
Binyamin <7raivis@inbox.lv>
2019-05-08 20:20:39 +0300

    Sentry v5.2.0

--- assets/ui.js
+++ assets/ui.js
@@ -202,9 +202,9 @@ window.ui = {
 
             // Sentry SDK https://docs.sentry.io/quickstart/?platform=browser
             // Latest version https://github.com/getsentry/sentry-javascript/releases
-            this.asyncScript("https://browser.sentry-cdn.com/5.1.2/bundle.min.js", {
+            this.asyncScript("https://browser.sentry-cdn.com/5.2.0/bundle.min.js", {
                 // Generator SRI hash https://www.srihash.org
-                integrity: "sha384-+/oUcWqoSlvX84sy3dZn9d+ruXMwNqkl52P1vv4Bl/SSwniu4Q2xqCiNc4LJyp5k",
+                integrity: "sha384-khQaIyAjM+/x4rFM4oqoXsmlzVmNfenGyoTz91AN/jrVU3StrjnsOOFGd+7x28Id",
                 crossorigin: "anonymous",
                 onSuccess: function () {
                     if (window.Sentry && Sentry.init) {
