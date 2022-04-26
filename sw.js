5c9c55e66af15468cc45bb64bf52de68c40b6424
Binyamin <laukstein@gmail.com>
2022-04-26 23:00:39 +0300

    Line ending LF, added EditorConfig

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 "use strict";
 
 addEventListener("install", () => skipWaiting());
+// addEventListener("activate", () => waitUntil(self.clients.claim());
 addEventListener("message", event => {
     clients.matchAll().then(clients => {
         // No need to update the tab that sent the data
@@ -8,3 +9,48 @@ addEventListener("message", event => {
     });
 });
 
+/* // Secure headers https://scotthelme.co.uk/security-headers-cloudflare-worker/
+// https://github.com/securityheaders/security-headers-cloudflare-worker
+const securityHeaders = {
+        "Content-Security-Policy": "upgrade-insecure-requests",
+        "Strict-Transport-Security": "max-age=1000",
+        "X-Frame-Options": "DENY",
+        // "X-Content-Type-Options": "nosniff",
+        "Referrer-Policy": "strict-origin-when-cross-origin"
+    },
+    sanitiseHeaders = {
+        Server: ""
+    },
+    removeHeaders = [
+        "Public-Key-Pins",
+        "X-Powered-By",
+        "X-AspNet-Version",
+        "etag"
+    ];
+
+async function addHeaders(req) {
+    console.log(req.url);
+
+    const url = new URL(req.url),
+        response = await fetch(req),
+        newHeaders = new Headers(response.headers);
+    var setHeaders = Object.assign({}, sanitiseHeaders);
+
+    if (url.origin === location.origin) {
+        if (newHeaders.has("Content-Type") && newHeaders.get("Content-Type").includes("text/html")) {
+            setHeaders = Object.assign({}, securityHeaders, sanitiseHeaders);
+        }
+
+        Object.keys(setHeaders).forEach(name => newHeaders.set(name, setHeaders[name]));
+        removeHeaders.forEach(name => newHeaders.delete(name));
+    }
+
+    return new Response(response.body, {
+        // undefined fixes "Uncaught RangeError" https://github.com/sorodrigo/redux-offline/issues/44#issuecomment-340031669
+        status: response.status || undefined,
+        statusText: response.statusText,
+        headers: newHeaders
+    });
+}
+
+addEventListener("fetch", event => event.respondWith(addHeaders(event.request))); */
